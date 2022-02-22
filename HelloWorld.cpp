7dddc9b855a67a09f21131d9733ce9831fe89f4b
Elly <elly@192.168.2.7>
2022-02-22 16:20:50 +0700

    PLP1 submission

---
+++ HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // To include the input-output stream library
+using namespace std; // To shorten the cout/cin code lines
+
+int main()
+{                                  // main function running
+    cout << "hello world" << endl; // print out to the console
+}
